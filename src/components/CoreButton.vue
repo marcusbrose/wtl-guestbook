23553a99d4e968aa0f274b43d328ef267d83c775
Marcus Brose <contact@marcusbrose.com>
2023-06-03 16:56:33 +0200

    core button component

---
+++ src/components/CoreButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">
+    <slot />
+  </button>
+</template>
