668a4ba86104859289318ad6416adf633995d5d3
Marcus Brose <contact@marcusbrose.com>
2023-06-03 18:38:25 +0200

    core button disabled styling

--- src/components/CoreButton.vue
+++ src/components/CoreButton.vue
@@ -1,5 +1,19 @@
 <template>
-  <button class="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">
+  <button class="core-button">
     <slot />
   </button>
 </template>
+
+<style>
+.core-button {
+  @apply flex justify-center items-center;
+  @apply px-3.5 py-2.5 text-sm font-semibold;
+  @apply bg-emerald-600 text-white rounded-md shadow-sm;
+  @apply hover:bg-emerald-500;
+  @apply focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600;
+}
+
+.core-button:disabled {
+  @apply bg-gray-300 text-gray-600;
+}
+</style>
