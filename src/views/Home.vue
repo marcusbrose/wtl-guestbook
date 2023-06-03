2ec03bd04feee80fc32ee8239dac5ac7baa40c83
Marcus Brose <contact@marcusbrose.com>
2023-06-03 16:59:20 +0200

    layout home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,21 @@
 <script setup>
 import MainLayout from '@/components/MainLayout.vue'
+import CoreTitle from '../components/CoreTitle.vue'
+import CoreButton from '../components/CoreButton.vue'
 </script>
 
 <template>
   <MainLayout>
-    <h1>Home</h1>
+    <div class="p-4 pt-12">
+      <CoreTitle title="Welcome to the guestbook app" />
+      
+      <div class="mt-10 flex items-center gap-x-6">
+        <RouterLink to="/guestbook">
+          <CoreButton>
+            Try now
+          </CoreButton>
+        </RouterLink>
+      </div>
+    </div>
   </MainLayout>
 </template>
