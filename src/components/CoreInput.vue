5b88e6b79ab940913783187b60c4d1ad4eb078fa
Marcus Brose <contact@marcusbrose.com>
2023-06-03 17:25:31 +0200

    create core input field

---
+++ src/components/CoreInput.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  id: {
+    type: String
+  },
+  modelValue: {
+    type: String
+  },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const onInput = (e) => {
+  emit('update:modelValue', e.target.value)
+}
+</script>
+
+<template>
+  <input type="text" :name="id" :id="id" :value="modelValue" @input="onInput"
+    class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6" />
+</template>
