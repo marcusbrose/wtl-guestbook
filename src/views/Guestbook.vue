c20405ce60ca6beceba0ef08dce914f0db6a148b
Marcus Brose <contact@marcusbrose.com>
2023-06-03 16:59:29 +0200

    layout guestbook page

--- src/views/Guestbook.vue
+++ src/views/Guestbook.vue
@@ -1,9 +1,17 @@
 <script setup>
 import MainLayout from '@/components/MainLayout.vue'
+import CoreTitle from '../components/CoreTitle.vue'
 </script>
 
 <template>
   <MainLayout>
-    <h1>Guestbook</h1>
+    <div class="flex pt-12">
+      <div class="flex-1 p-4">
+        <CoreTitle title="Guestbook" subtitle="Read what our customers think." />
+      </div>
+      <div class="flex-1 p-4">
+        <CoreTitle title="Write a few words for us" />
+      </div>
+    </div>
   </MainLayout>
 </template>
