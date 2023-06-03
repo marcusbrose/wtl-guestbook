236ae1394d9162c9156aa3b479f3cfd77d3933c3
Marcus Brose <contact@marcusbrose.com>
2023-06-03 16:56:45 +0200

    core title component

---
+++ src/components/CoreTitle.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+  },
+  subtitle: {
+    type: String,
+    required: false,
+  }
+})
+</script>
+
+<template>
+  <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+    {{ title }}
+  </h2>
+  <p v-if="subtitle" class="mt-2 text-lg leading-8 text-gray-600">
+    {{ subtitle }}
+  </p>
+</template>
