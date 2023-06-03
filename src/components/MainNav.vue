55581afc199d94628b05eb05c0b3f834bde90cb1
Marcus Brose <contact@marcusbrose.com>
2023-06-03 16:57:36 +0200

    make main nav dynamic

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,6 +1,17 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+const navigation = [
+  { name: 'Home', path: '/' },
+  { name: 'Guestbook', path: '/guestbook' },
+]
+</script>
+
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/guestbook">Guestbook</RouterLink>
+  <nav class="flex space-x-4">
+    <RouterLink v-for="item in navigation" :key="item.name" :to="item.path"
+      class="text-sm font-semibold leading-6 text-gray-900">
+      {{ item.name }}
+    </RouterLink>
   </nav>
 </template>
