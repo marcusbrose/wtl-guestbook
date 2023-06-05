6445501f30e32cb19e92a10ace61bada4d9ba1d1
Marcus Brose <contact@marcusbrose.com>
2023-06-05 08:09:54 +0200

    refactor guestbook item styles

--- src/components/GuestbookListItem.vue
+++ src/components/GuestbookListItem.vue
@@ -25,16 +25,34 @@ const text = computed(() => {
 </script>
 
 <template>
-  <article class="flex flex-col items-start justify-between space-y-2 py-8">
-    <time :datetime="entry.createdAt" class="text-gray-500">
+  <article class="guestbook-item">
+    <time :datetime="entry.createdAt" class="guestbook-item__time">
       {{ datetime }}
     </time>
-    <h3 class="text-lg font-semibold leading-6 text-gray-900">
+    <h3 class="guestbook-item__title">
       {{ entry.title }}
     </h3>
-    <p v-html="text" class="line-clamp-3 text-base leading-6 text-gray-600" />
-    <p class="text-sm relative z-10 rounded-full font-medium text-gray-400">
+    <p v-html="text" class="guestbook-item__text" />
+    <p class="guestbook-item__by">
       by {{ entry.email }}
     </p>
   </article>
 </template>
+
+<style>
+.guestbook-item {
+  @apply flex flex-col items-start justify-between space-y-2 py-8;
+}
+.guestbook-item__time {
+  @apply text-gray-500;
+}
+.guestbook-item__title {
+  @apply text-lg font-semibold leading-6 text-gray-900;
+}
+.guestbook-item__text {
+  @apply line-clamp-3 text-base leading-6 text-gray-600;
+}
+.guestbook-item__by {
+  @apply text-sm relative z-10 rounded-full font-medium text-gray-400;
+}
+</style>
